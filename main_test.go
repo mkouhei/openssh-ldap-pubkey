c557124842929c696ceb4af43bbfb7a52301424e
Kouhei Maeda <mkouhei@palmtb.net>
2015-10-14 23:11:28 +0900

    Adds tests with slapd.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- main_test.go
+++ main_test.go
@@ -53,3 +53,21 @@ func TestIsAddrWithFQDN(t *testing.T) {
 		t.Fatalf("expecting: %s is false, but true", host)
 	}
 }
+
+func TestConnect(t *testing.T) {
+	l := &ldapEnv{"localhost", 389, "dc=example,dc=org", defaultFilter, false, ""}
+	c := l.connect()
+	if c == nil {
+		t.Fatal("Connect error")
+	}
+}
+
+func Example_PrintPubkey() {
+	l := &ldapEnv{"localhost", 389, "dc=example,dc=org", defaultFilter, false, ""}
+	c := l.connect()
+	simpleBind(c)
+	printPubkey(l.search(c))
+	// Output:
+	// ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8OldtAiW9lQ0/2VJcc9UpRW9nfcusGXEu2sS+p5kh05zTYWGd8xHgZD0vfoQfpTfSKuHsL6qlMyKQMfsULWQoMJmMhJZc2hU1LH4u9HXYwJxD7EFleGTfxgYw6F6+LWHPVTTyhq+oMgXp/qfE4lc5A0xd2En9Qc172naHD+cRHZxhfNNYEGhW7E6eYm02Gn4fBN8hSpuZzv3WlpRgFiAWGv9CqObdQUEFFnpYLnC2kmaHqz8lzkZ9c3jdJMn2zPYyDAqQ52GI8EKyX9SrbepGJUaa/DmGyEg8nIBu4U74Sigfcl6dsJmA2qlOqSxia21mnQEFiSARB74pakgiywFV user0@workstation
+	// ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrMQOAP3o58yl96HjEsheDAO/qgQ/mLVJK7DW+VFbJ9dGJpJfB4CBXPoT9bfSn4y6dotqjBA1eDbpDyzrhLkIe1MWZrRjkFbzAtB54ydKSU48URsb+XtGnN6kKKpipolQRvr3CRV7Yu2ELJDq+9Oz1gILK4nc1W/iaORVO/tZRPA0vdQwP0qkUf//neUmXXbSxOSm+ekQvZI9KfJ2tWxe+mVSFt+PcC2P4A/bW9dCNplqZdFTMQxLYFpl5ZOz3fwWcy34Shcb5nSZbjpKZdNrpuUCLwq2FMxorupko8kf4RmvMYO3G6p6OqpoIt6raB8DDJ+v/f6jdgPA31HK0sejX user0@vm01
+}
