3df7238c46c1e5bfc1399698d61ba0634cdd63cf
Kouhei Maeda <mkouhei@palmtb.net>
2018-09-29 11:36:40 +0900

    Refactors.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ ldap_test.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestIsAddrWithV4(t *testing.T) {
+	addr := "192.0.2.100"
+	if !isAddr(addr) {
+		t.Fatalf("expecting: %s is true, but false", addr)
+	}
+}
+
+func TestIsAddrWithV6(t *testing.T) {
+	addr := "2001:db8::100"
+	if !isAddr(addr) {
+		t.Fatalf("expecting: %s is true, but false", addr)
+	}
+}
+
+func TestIsAddrWithFQDN(t *testing.T) {
+	host := "ldap.example.org"
+	if isAddr(host) {
+		t.Fatalf("expecting: %s is false, but true", host)
+	}
+}
+
+func TestConnect(t *testing.T) {
+	l := &ldapEnv{"localhost", 389, "dc=example,dc=org", defaultFilter, false, false, false, "", "", ""}
+	if _, err := l.connect(); err != nil {
+		t.Fatal("Connect error")
+	}
+}
+
+func TestConnectFail(t *testing.T) {
+	l := &ldapEnv{"localhost", 9999, "dc=example,dc=org", defaultFilter, false, false, false, "", "", ""}
+	if _, err := l.connect(); err == nil {
+		t.Fatal("expecting fail to error.")
+	}
+}
+
+func TestConnectTLS(t *testing.T) {
+	l := &ldapEnv{"localhost", 636, "dc=example,dc=org", defaultFilter, true, true, false, "", "", ""}
+	if _, err := l.connectTLS(); err != nil {
+		t.Fatal("Connect error")
+	}
+}
+
+func TestBind(t *testing.T) {
+	l := &ldapEnv{"localhost", 389, "dc=example,dc=org", defaultFilter, false, false, false, "", "cn=admin,dc=example,dc=org", "password"}
+	c, _ := l.connect()
+	if err := simpleBind(c, l); err != nil {
+		t.Fatal("Bind error")
+	}
+}
+
+func TestBindFail(t *testing.T) {
+	l := &ldapEnv{"localhost", 389, "dc=example,dc=org", defaultFilter, false, false, false, "", "cn=admin,dc=example,dc=org", ""}
+	c, _ := l.connect()
+	if err := simpleBind(c, l); err == nil {
+		t.Fatal("Bind error")
+	}
+}
+
+func PrintPubkey() {
+	l := &ldapEnv{"localhost", 389, "dc=example,dc=org", defaultFilter, false, false, false, "user0", "", ""}
+	c, _ := l.connect()
+	simpleBind(c, l)
+	entries, _ := l.search(c)
+	printPubkey(entries)
+	// Output:
+	// ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8OldtAiW9lQ0/2VJcc9UpRW9nfcusGXEu2sS+p5kh05zTYWGd8xHgZD0vfoQfpTfSKuHsL6qlMyKQMfsULWQoMJmMhJZc2hU1LH4u9HXYwJxD7EFleGTfxgYw6F6+LWHPVTTyhq+oMgXp/qfE4lc5A0xd2En9Qc172naHD+cRHZxhfNNYEGhW7E6eYm02Gn4fBN8hSpuZzv3WlpRgFiAWGv9CqObdQUEFFnpYLnC2kmaHqz8lzkZ9c3jdJMn2zPYyDAqQ52GI8EKyX9SrbepGJUaa/DmGyEg8nIBu4U74Sigfcl6dsJmA2qlOqSxia21mnQEFiSARB74pakgiywFV user0@workstation
+	// ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrMQOAP3o58yl96HjEsheDAO/qgQ/mLVJK7DW+VFbJ9dGJpJfB4CBXPoT9bfSn4y6dotqjBA1eDbpDyzrhLkIe1MWZrRjkFbzAtB54ydKSU48URsb+XtGnN6kKKpipolQRvr3CRV7Yu2ELJDq+9Oz1gILK4nc1W/iaORVO/tZRPA0vdQwP0qkUf//neUmXXbSxOSm+ekQvZI9KfJ2tWxe+mVSFt+PcC2P4A/bW9dCNplqZdFTMQxLYFpl5ZOz3fwWcy34Shcb5nSZbjpKZdNrpuUCLwq2FMxorupko8kf4RmvMYO3G6p6OqpoIt6raB8DDJ+v/f6jdgPA31HK0sejX user0@vm01
+}
+
+func PrintPubkeyTLS() {
+	l := &ldapEnv{"localhost", 636, "dc=example,dc=org", defaultFilter, true, true, false, "user0", "", ""}
+	c, _ := l.connectTLS()
+	simpleBind(c, l)
+	entries, _ := l.search(c)
+	printPubkey(entries)
+	// Output:
+	// ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8OldtAiW9lQ0/2VJcc9UpRW9nfcusGXEu2sS+p5kh05zTYWGd8xHgZD0vfoQfpTfSKuHsL6qlMyKQMfsULWQoMJmMhJZc2hU1LH4u9HXYwJxD7EFleGTfxgYw6F6+LWHPVTTyhq+oMgXp/qfE4lc5A0xd2En9Qc172naHD+cRHZxhfNNYEGhW7E6eYm02Gn4fBN8hSpuZzv3WlpRgFiAWGv9CqObdQUEFFnpYLnC2kmaHqz8lzkZ9c3jdJMn2zPYyDAqQ52GI8EKyX9SrbepGJUaa/DmGyEg8nIBu4U74Sigfcl6dsJmA2qlOqSxia21mnQEFiSARB74pakgiywFV user0@workstation
+	// ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrMQOAP3o58yl96HjEsheDAO/qgQ/mLVJK7DW+VFbJ9dGJpJfB4CBXPoT9bfSn4y6dotqjBA1eDbpDyzrhLkIe1MWZrRjkFbzAtB54ydKSU48URsb+XtGnN6kKKpipolQRvr3CRV7Yu2ELJDq+9Oz1gILK4nc1W/iaORVO/tZRPA0vdQwP0qkUf//neUmXXbSxOSm+ekQvZI9KfJ2tWxe+mVSFt+PcC2P4A/bW9dCNplqZdFTMQxLYFpl5ZOz3fwWcy34Shcb5nSZbjpKZdNrpuUCLwq2FMxorupko8kf4RmvMYO3G6p6OqpoIt6raB8DDJ+v/f6jdgPA31HK0sejX user0@vm01
+}
+
+func PrintPubkeyDoesNotUseSSHPublicKey() {
+	l := &ldapEnv{"localhost", 389, "dc=example,dc=org", defaultFilter, false, false, false, "user2", "", ""}
+	c, _ := l.connect()
+	simpleBind(c, l)
+	entries, _ := l.search(c)
+	printPubkey(entries)
+	// Output:
+	//
+}
+
+func PrintPubkeyDoesNotExistUser() {
+	l := &ldapEnv{"localhost", 389, "dc=example,dc=org", defaultFilter, false, false, false, "user5", "", ""}
+	c, _ := l.connect()
+	simpleBind(c, l)
+	entries, _ := l.search(c)
+	printPubkey(entries)
+	// Output:
+	//
+}
--- main.go
+++ main.go
@@ -5,11 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net"
 	"os"
-
-	"crypto/tls"
-	"crypto/x509"
 
 	"gopkg.in/ldap.v2"
 )
@@ -18,19 +14,6 @@ const (
 	version          = "0.1.3"
 	sshPublicKeyName = "sshPublicKey"
 )
-
-type ldapEnv struct {
-	host   string
-	port   int
-	base   string
-	filter string
-	tls    bool
-	skip   bool
-	debug  bool
-	uid    string
-	binddn string
-	bindpw string
-}
 
 var (
 	ver        string
@@ -90,83 +73,6 @@ func (l *ldapEnv) argparse(args []string, ver string) error {
 		return errors.New("Specify username")
 	}
 	l.uid = flags.Args()[0]
-	return nil
-}
-
-func isAddr(host string) bool {
-	return !(net.ParseIP(host).To4() == nil && net.ParseIP(host).To16() == nil)
-}
-
-func (l *ldapEnv) getAddr() string {
-	addrs, err := net.LookupHost(l.host)
-	if err != nil {
-		return l.host
-	}
-	if net.ParseIP(addrs[0]).To16() != nil {
-		return fmt.Sprintf("[%s]", addrs[0])
-	}
-	return addrs[0]
-}
-
-func (l *ldapEnv) connect() (*ldap.Conn, error) {
-	host := l.host
-	if !isAddr(host) {
-		host = l.getAddr()
-	}
-	return ldap.Dial("tcp", fmt.Sprintf("%s:%d", host, l.port))
-}
-
-func (l *ldapEnv) connectTLS() (*ldap.Conn, error) {
-	certs := *x509.NewCertPool()
-	tlsConfig := &tls.Config{
-		RootCAs: &certs,
-	}
-
-	if isAddr(l.host) || l.skip {
-		tlsConfig.InsecureSkipVerify = true
-	}
-	host := l.host
-	if !isAddr(l.host) {
-		tlsConfig.ServerName = l.host
-		host = l.getAddr()
-	}
-	return ldap.DialTLS("tcp", fmt.Sprintf("%s:%d", host, l.port), tlsConfig)
-}
-
-func logging(err error) {
-	if err == errVersion {
-		os.Exit(0)
-	} else if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func simpleBind(c *ldap.Conn, l *ldapEnv) error {
-	bindRequest := ldap.NewSimpleBindRequest(l.binddn, l.bindpw, nil)
-	_, err := c.SimpleBind(bindRequest)
-	return err
-}
-
-func (l *ldapEnv) search(c *ldap.Conn) ([]*ldap.Entry, error) {
-	searchRequest := ldap.NewSearchRequest(
-		l.base, ldap.ScopeWholeSubtree, ldap.NeverDerefAliases,
-		0, 0, false,
-		fmt.Sprintf(l.filter, l.uid), []string{sshPublicKeyName}, nil)
-	sr, err := c.Search(searchRequest)
-	return sr.Entries, err
-}
-
-func printPubkey(entries []*ldap.Entry) error {
-	if len(entries) != 1 {
-		return errors.New("User does not exist or too many entries returned")
-	}
-
-	if len(entries[0].GetAttributeValues("sshPublicKey")) == 0 {
-		return errors.New("User does not use ldapPublicKey")
-	}
-	for _, pubkey := range entries[0].GetAttributeValues("sshPublicKey") {
-		fmt.Println(pubkey)
-	}
 	return nil
 }
 
